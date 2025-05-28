26fceaa73d49b62109759bab47c23b74671eefbd
nitinkalia9 <nkalia421@gmail.com>
2025-05-28 16:52:59 +0530

    Problem table and submission component added and some bug fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import { Loader } from "lucide-react";
 import Layout from "./layout/Layout.jsx";
 import AdminRoute from "./components/AdminRoute.jsx";
 import AddProblem from "./page/AddProblem.jsx";
-import ProblemPage from "./page/ProblemPage.js";
+import ProblemPage from "./page/ProblemPage.jsx";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
